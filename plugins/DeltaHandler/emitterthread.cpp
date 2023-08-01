6147dc4c39b147fd04b6adf23b1c026302c08fd7
Lothar Ketterer <deltatouch@mailbox.org>
2023-08-01 19:54:24 +0200

    added missing events; changed output to one line per event

--- plugins/DeltaHandler/emitterthread.cpp
+++ plugins/DeltaHandler/emitterthread.cpp
@@ -37,44 +37,38 @@ void EmitterThread::run()
             eventType = dc_event_get_id(event);
             switch (eventType) {
                 case DC_EVENT_CHAT_EPHEMERAL_TIMER_MODIFIED:
-                    qDebug() << "Emitter: DC_EVENT_CHAT_EPHEMERAL_TIMER_MODIFIED";
-                    qDebug() << "Emitter: chat_id: " << dc_event_get_data1_int(event);
-                    qDebug() << "Emitter: timer val in s or 0 for disabled timer: " << dc_event_get_data2_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_CHAT_EPHEMERAL_TIMER_MODIFIED" << ", account " << dc_event_get_account_id(event) << ", chat_id: " << dc_event_get_data1_int(event) << ", timer val in s or 0 for disabled timer: " << dc_event_get_data2_int(event);
                     break;
 
                 case DC_EVENT_CHAT_MODIFIED:
-                    qDebug() << "Emitter: DC_EVENT_CHAT_MODIFIED";
-                    qDebug() << "Emitter: chat id: " << dc_event_get_data1_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_CHAT_MODIFIED" << ", account " << dc_event_get_account_id(event) << ", chat id: " << dc_event_get_data1_int(event);
                     emit chatDataModified(dc_event_get_account_id(event), dc_event_get_data1_int(event));
                     break;
 
                 case DC_EVENT_CONFIGURE_PROGRESS:
-                    qDebug() << "Emitter: DC_EVENT_CONFIGURE_PROGRESS";
-                    qDebug() << "Emitter: Progress from 1 - 1000, 0 = error: " << dc_event_get_data1_int(event);
                     eventData2Str = dc_event_get_data2_str(event);
                     if (eventData2Str) {
-                        qDebug() << "Emitter: Comment/Error: " << eventData2Str;
                         data2info = eventData2Str;
                     }
                     else {
                         data2info = "";
                     }
+                    qInfo().nospace() << "Emitter: DC_EVENT_CONFIGURE_PROGRESS" << ", account " << dc_event_get_account_id(event) << ", progress from 1 - 1000, 0 = error: " << dc_event_get_data1_int(event) << ", comment/error: " << qUtf8Printable(data2info);
                     emit configureProgress(dc_event_get_data1_int(event), data2info);
                     break;
 
                 case DC_EVENT_CONNECTIVITY_CHANGED:
-                    qDebug() << "Emitter: DC_EVENT_CONNECTIVITY_CHANGED";
+                    qInfo().nospace() << "Emitter: DC_EVENT_CONNECTIVITY_CHANGED" << ", account " << dc_event_get_account_id(event);
                     break;
                     
                 case DC_EVENT_CONTACTS_CHANGED:
-                    qDebug() << "Emitter: DC_EVENT_CONTACTS_CHANGED";
+                    qInfo().nospace() << "Emitter: DC_EVENT_CONTACTS_CHANGED" << ", account " << dc_event_get_account_id(event);
                     emit contactsChanged();
                     break;
                     
                 case DC_EVENT_DELETED_BLOB_FILE:
-                    qDebug() << "Emitter: DC_EVENT_DELETED_BLOB_FILE";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: Path name: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_DELETED_BLOB_FILE" << ", account " << dc_event_get_account_id(event) << ", path name: " << qUtf8Printable(eventData2Str);
                     break;
                     
                 case DC_EVENT_ERROR:
@@ -90,177 +84,149 @@ void EmitterThread::run()
                      * showing these events until the function has really failed (returned false). It
                      * should be sufficient to report only the last error in a message box then.
                      */
-                    qDebug() << "Emitter: DC_EVENT_ERROR";
                     eventData2Str = dc_event_get_data2_str(event); 
-                    qDebug() << "Emitter: " << eventData2Str;
-
+                    qInfo().nospace() << "Emitter: DC_EVENT_ERROR" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     emit errorEvent(eventData2Str);
-
                     break;
                     
                 case DC_EVENT_ERROR_SELF_NOT_IN_GROUP:
-                    qDebug() << "Emitter: DC_EVENT_ERROR_SELF_NOT_IN_GROUP";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_ERROR_SELF_NOT_IN_GROUP" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
                     
                 case DC_EVENT_IMAP_CONNECTED:
-                    qDebug() << "Emitter: DC_EVENT_IMAP_CONNECTED";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_IMAP_CONNECTED" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
                     
+                case DC_EVENT_IMAP_INBOX_IDLE:
+                    qInfo().nospace() << "Emitter: DC_EVENT_IMAP_INBOX_IDLE" << ", account " << dc_event_get_account_id(event);
+                    break;
+
                 case DC_EVENT_IMAP_MESSAGE_DELETED:
-                    qDebug() << "Emitter: DC_EVENT_IMAP_MESSAGE_DELETED";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_IMAP_MESSAGE_DELETED" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
                     
                 case DC_EVENT_IMAP_MESSAGE_MOVED:
-                    qDebug() << "Emitter: DC_EVENT_IMAP_MESSAGE_MOVED";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_IMAP_MESSAGE_MOVED" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
                     
                 case DC_EVENT_IMEX_FILE_WRITTEN:
-                    qDebug() << "Emitter: DC_EVENT_IMEX_FILE_WRITTEN";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_IMEX_FILE_WRITTEN" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     data2info = eventData2Str;
                     emit imexFileWritten(data2info);
                     break;
                     
                 case DC_EVENT_IMEX_PROGRESS:
-                    qDebug() << "Emitter: DC_EVENT_IMEX_PROGRESS";
-                    qDebug() << "Emitter: Progress from 1 - 1000, 0 = error: " << dc_event_get_data1_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_IMEX_PROGRESS" << ", account " << dc_event_get_account_id(event) << ", progress from 1 - 1000, 0 = error: " << dc_event_get_data1_int(event);
                     emit imexProgress(dc_event_get_data1_int(event));
                     break;
                     
                 case DC_EVENT_INCOMING_MSG:
-                    qDebug() << "Emitter: DC_EVENT_INCOMING_MSG";
-                    qDebug() << "Emitter: account_id: " << dc_event_get_account_id(event);
-                    qDebug() << "Emitter: chat_id: " << dc_event_get_data1_int(event);
-                    qDebug() << "Emitter: msg_id: " << dc_event_get_data2_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_INCOMING_MSG" << ", account " << dc_event_get_account_id(event) << ", chat_id: " << dc_event_get_data1_int(event) << ", msg_id: " << dc_event_get_data2_int(event);
                     emit newMsg(dc_event_get_account_id(event), dc_event_get_data1_int(event), dc_event_get_data2_int(event));
                     break;
                     
-//                    // not implemented yet in the version of the lib we use
-//                case DC_EVENT_INCOMING_MSG_BUNCH:
-//                    // TODO? 
-//                    qDebug() << "Emitter: DC_EVENT_INCOMING_MSG_BUNCH";
-//                    eventData2Str = dc_event_get_data2_str(event);
-//                    qDebug() << "Emitter: " << eventData2Str;
-//                    break;
+                case DC_EVENT_INCOMING_MSG_BUNCH:
+                    eventData2Str = dc_event_get_data2_str(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_INCOMING_MSG_BUNCH" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
+                    // TODO: eventData2Str contains a json with the messge IDs.
+                    // From the DC documentation:
+                    // This is an event to allow the UI to only show one notification per
+                    // message bunch, instead of cluttering the user with many
+                    // notifications. For each of the msg_ids, an additional
+                    // DC_EVENT_INCOMING_MSG event was emitted before.
+                    break;
                     
                 case DC_EVENT_INFO: 
-                    qDebug() << "Emitter: DC_EVENT_INFO";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_INFO" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
 
                 case DC_EVENT_LOCATION_CHANGED:
-                    qDebug() << "Emitter: DC_EVENT_LOCATION_CHANGED";
-                    qDebug() << "Emitter: contact_id: " << dc_event_get_data1_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_LOCATION_CHANGED" << ", account " << dc_event_get_account_id(event) << ", contact_id: " << dc_event_get_data1_int(event);
                     break;
 
+                case DC_EVENT_MSG_DELETED:
+                    qInfo().nospace() << "Emitter: DC_EVENT_MSG_DELETED" << ", account " << dc_event_get_account_id(event) << ", chat_id: " << dc_event_get_data1_int(event) << ", msg_id: " << dc_event_get_data2_int(event);
+                    break;
+
+
                 case DC_EVENT_MSG_DELIVERED:
-                    /* TODO */
-                    qDebug() << "Emitter: DC_EVENT_MSG_DELIVERED";
-                    qDebug() << "Emitter: chat_id: " << dc_event_get_data1_int(event);
-                    qDebug() << "Emitter: msg_id: " << dc_event_get_data2_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_MSG_DELIVERED" << ", account " << dc_event_get_account_id(event) << ", chat_id: " << dc_event_get_data1_int(event) << ", msg_id: " << dc_event_get_data2_int(event);
                     emit msgDelivered(dc_event_get_account_id(event), dc_event_get_data1_int(event), dc_event_get_data2_int(event));
                     break;
 
                 case DC_EVENT_MSG_FAILED:
-                    qDebug() << "Emitter: DC_EVENT_MSG_FAILED";
-                    qDebug() << "Emitter: chat_id: " << dc_event_get_data1_int(event);
-                    qDebug() << "Emitter: msg_id: " << dc_event_get_data2_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_MSG_FAILED" << ", account " << dc_event_get_account_id(event) << ", chat_id: " << dc_event_get_data1_int(event) << ", msg_id: " << dc_event_get_data2_int(event);
                     emit msgFailed(dc_event_get_account_id(event), dc_event_get_data1_int(event), dc_event_get_data2_int(event));
                     break;
 
                 case DC_EVENT_MSG_READ:
-                    qDebug() << "Emitter: DC_EVENT_MSG_READ";
-                    qDebug() << "Emitter: account_id: " << dc_event_get_account_id(event);
-                    qDebug() << "Emitter: chat_id: " << dc_event_get_data1_int(event);
-                    qDebug() << "Emitter: msg_id: " << dc_event_get_data2_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_MSG_READ" << ", account " << dc_event_get_account_id(event) << ", chat_id: " << dc_event_get_data1_int(event) << ", msg_id: " << dc_event_get_data2_int(event);
                     emit msgRead(dc_event_get_account_id(event), dc_event_get_data1_int(event), dc_event_get_data2_int(event));
                     break;
 
                 case DC_EVENT_MSGS_CHANGED:
-                    qDebug() << "Emitter: DC_EVENT_MSGS_CHANGED";
-                    qDebug() << "Emitter: account_id: " << dc_event_get_account_id(event);
-                    qDebug() << "Emitter: chat_id (if multiple: 0): " << dc_event_get_data1_int(event);
-                    qDebug() << "Emitter: msg_id (if multiple: 0): " << dc_event_get_data2_int(event);
-                    // TODO: check if it is sufficient to handle this event to update the
-                    // chat view when we have sent a message
-                    // or maybe it should be a different signal?
+                    qInfo().nospace() << "Emitter: DC_EVENT_MSGS_CHANGED" << ", account " << dc_event_get_account_id(event) << ", chat_id (if multiple: 0): " << dc_event_get_data1_int(event) << ", msg_id (if multiple: 0): " << dc_event_get_data2_int(event);
                     emit msgsChanged(dc_event_get_account_id(event), dc_event_get_data1_int(event), dc_event_get_data2_int(event));
                     break;
 
                 case DC_EVENT_MSGS_NOTICED:
-                    qDebug() << "Emitter: DC_EVENT_MSGS_NOTICED";
-                    qDebug() << "Emitter: chat_id: " << dc_event_get_data1_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_MSGS_NOTICED" << ", account " << dc_event_get_account_id(event) << ", chat_id: " << dc_event_get_data1_int(event);
                     break;
 
                 case DC_EVENT_NEW_BLOB_FILE:
-                    qDebug() << "Emitter: DC_EVENT_NEW_BLOB_FILE";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_NEW_BLOB_FILE" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
 
-//                    // not implemented yet in the version of the lib we use
-//                case DC_EVENT_REACTIONS_CHANGED:
-//                    qDebug() << "Emitter: chat_id: " << dc_event_get_data1_int(event);
-//                    qDebug() << "Emitter: msg_id: " << dc_event_get_data2_int(event);
-//                    break;
+                case DC_EVENT_REACTIONS_CHANGED:
+                    qInfo().nospace() << "Emitter: DC_EVENT_REACTIONS_CHANGED" << ", account " << dc_event_get_account_id(event) << ", chat_id: " << dc_event_get_data1_int(event) << ", msg_id: " << dc_event_get_data2_int(event);
+                    break;
 
                 case DC_EVENT_SECUREJOIN_INVITER_PROGRESS:
-                    qDebug() << "Emitter: DC_EVENT_SECUREJOIN_INVITER_PROGRESS";
-                    qDebug() << "Emitter: contact_id: " << dc_event_get_data1_int(event);
-                    qDebug() << "Emitter: progress: " << dc_event_get_data2_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_SECUREJOIN_INVITER_PROGRESS" << ", account " << dc_event_get_account_id(event) << ", contact_id: " << dc_event_get_data1_int(event) << ", progress: " << dc_event_get_data2_int(event);
                     break;
 
                 case DC_EVENT_SECUREJOIN_JOINER_PROGRESS:
-                    qDebug() << "Emitter: DC_EVENT_SECUREJOIN_JOINER_PROGRESS";
-                    qDebug() << "Emitter: contact_id: " << dc_event_get_data1_int(event);
-                    qDebug() << "Emitter: progress: " << dc_event_get_data2_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_SECUREJOIN_JOINER_PROGRESS" << ", account " << dc_event_get_account_id(event) << ", contact_id: " << dc_event_get_data1_int(event) << ", progress: " << dc_event_get_data2_int(event);
                     break;
 
                 case DC_EVENT_SELFAVATAR_CHANGED: 
-                    qDebug() << "Emitter: DC_EVENT_SELFAVATAR_CHANGED";
+                    qInfo().nospace() << "Emitter: DC_EVENT_SELFAVATAR_CHANGED" << ", account " << dc_event_get_account_id(event);
                     break;
 
                 case DC_EVENT_SMTP_CONNECTED:
-                    qDebug() << "Emitter: DC_EVENT_SMTP_CONNECTED";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_SMTP_CONNECTED" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
 
                 case DC_EVENT_SMTP_MESSAGE_SENT:
-                    qDebug() << "Emitter: DC_EVENT_SMTP_MESSAGE_SENT";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_SMTP_MESSAGE_SENT" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
 
                 case DC_EVENT_WARNING:
-                    qDebug() << "Emitter: DC_EVENT_WARNING";
                     eventData2Str = dc_event_get_data2_str(event);
-                    qDebug() << "Emitter: " << eventData2Str;
+                    qInfo().nospace() << "Emitter: DC_EVENT_WARNING" << ", account " << dc_event_get_account_id(event) << ": " << qUtf8Printable(eventData2Str);
                     break;
 
                 case DC_EVENT_WEBXDC_INSTANCE_DELETED:
-                    qDebug() << "Emitter: DC_EVENT_WEBXDC_INSTANCE_DELETED";
-                    qDebug() << "Emitter: msg_id: " << dc_event_get_data1_int(event);
+                    qInfo().nospace() << "Emitter: DC_EVENT_WEBXDC_INSTANCE_DELETED" << ", account " << dc_event_get_account_id(event) << ", msg_id: " << dc_event_get_data1_int(event);
                     break;
 
                 case DC_EVENT_WEBXDC_STATUS_UPDATE:
-                    qDebug() << "Emitter: DC_EVENT_WEBXDC_STATUS_UPDATE";
-                    // has parameters, but at least one of them must
-                    // not be queried.
+                    qInfo().nospace() << "Emitter: DC_EVENT_WEBXDC_STATUS_UPDATE" << ", account " << dc_event_get_account_id(event);
+                    // has parameters, but at least one of them (data2) must
+                    // not be queried to avoid "races in the status replication".
                     break;
 
                 default:
-                    qDebug() << "Emitter: Unknown event received: " << eventType;
+                    qInfo().nospace() << "Emitter: Unknown event received" << ", account " << dc_event_get_account_id(event) << ": " << eventType;
             }
 
 

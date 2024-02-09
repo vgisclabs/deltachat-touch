6d77f7a2fb2c7040999706e73da1b81486f8b148
Lothar Ketterer <deltatouch@mailbox.org>
2024-02-09 20:14:14 +0100

    clean up includes

--- plugins/DeltaHandler/deltahandler.cpp
+++ plugins/DeltaHandler/deltahandler.cpp
@@ -16,10 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <vector>
 #include <fstream>
 #include "deltahandler.h"
-#include "quirc.h"
 //#include <unistd.h> // for sleep
 #include <QtDBus/QDBusConnection>
 #include <QtDBus/QDBusMessage>

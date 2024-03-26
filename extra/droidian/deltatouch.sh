7c7cedb5979097f34bf435525685a01d37330631
Lothar Ketterer <deltatouch@mailbox.org>
2024-03-26 21:35:42 +0100

    use different path for qml plugins

--- extra/droidian/deltatouch.sh
+++ extra/droidian/deltatouch.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd /home/droidian/bin/deltatouch
-#export QT_IM_MODULE=qtvirtualkeyboard
-QT_QPA_PLATFORMTHEME=qt5ct OPEN_OSK_VIA_DBUS=1 QML2_IMPORT_PATH=/usr/lib/aarch64-linux-gnu/ ./deltatouch 
+QT_QPA_PLATFORMTHEME=qt5ct OPEN_OSK_VIA_DBUS=1 QML2_IMPORT_PATH=/usr/local/lib/ ./deltatouch 
